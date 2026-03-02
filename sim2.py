43d8b0824417683017ccca82d9aaaa6320e2285e
{{your_name}} <{{your_email}}>
2026-03-02 17:37:56 +0530

    change sim2

--- sim2.py
+++ sim2.py
@@ -1,5 +1,18 @@
+# import sys
+# import os
+# import subprocess
+
+# # On macOS, launch_passive requires mjpython. Re-launch under it if needed.
+# if sys.platform == "darwin" and os.path.basename(sys.executable) != "mjpython":
+#     mjpython = os.path.join(os.path.dirname(sys.executable), "mjpython")
+#     sys.exit(subprocess.call([mjpython, __file__] + sys.argv[1:]))
+
 import mujoco
+import mujoco.viewer
 
 # Loading a specific model description as an imported module.
-from robot_descriptions import panda_mj_description
-model = mujoco.MjModel.from_xml_path(panda_mj_description.MJCF_PATH)
+from robot_descriptions import g1_description
+m = mujoco.MjModel.from_xml_path(g1_description.URDF_PATH)
+d = mujoco.MjData(m)
+
+mujoco.viewer.launch(m, d)
